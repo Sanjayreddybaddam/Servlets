dd39de2f71ae56e32710258a38209b09c9d8f14e
sanjay <you@example.com>
2024-10-26 19:04:00 +0530

    first commit

--- src/main/java/Servlet/HttpOne.java
+++ src/main/java/Servlet/HttpOne.java
@@ -11,14 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/httpone")
 public class HttpOne extends HttpServlet {
 	
-//	@Override
-//	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		String name = req.getParameter("username");
-//		
-//		req.setAttribute("name", name);
-//		
-//		req.getRequestDispatcher("httptwo").forward(req, resp);
-//	}
+
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
